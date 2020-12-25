d6a9b1000f593e73708d8c627ee93d0246b664cb
Aliev Ilya <aliev.ilia@yandex.ru>
2020-12-25 16:21:25 +0300

    train to validation split

---
+++ XML/train_toval.py
@@ -0,0 +1,13 @@
+
+def split_txtfile():
+    count = 0
+    with open('full.txt', 'r') as d:
+        strr = d.readlines()
+        with open('valid.txt', 'w+') as w:
+            with open('train.txt', 'w+') as o:
+                for i in strr:
+                    if count % 7 == 0:
+                        w.write(i)
+                    else:
+                        o.write(i)
+                    count += 7
